d0662e55c2b13ae8f337cfc4f845f0daf3663d62
baovn1179 <baovn1179@gmail.com>
2026-05-29 15:05:50 +0700

    Data export update

--- backend/controllers/ExportController.js
+++ backend/controllers/ExportController.js
@@ -1,4 +1,5 @@
 const DataModel = require('../models/DataModel');
+const db = require("../utils/db");
 
 class ExportController {
     static async ExportData(req, res) {
@@ -6,13 +7,80 @@ class ExportController {
             const data = req.body;
             await DataModel.ExportData(data, req, res);
             console.log("Data exported successfully");
-            return res.status(200).json({ message: "Dữ liệu xuất thành công" });
         } catch (error) {
             console.error("Error exporting data:", error);
             res.status(500).json({ message: "Có lỗi xảy ra khi xuất dữ liệu" });
         }
     }
+
+    static async ExportUsers(filter = [], req, res) {
+        /*
+        filter = [{
+        key: "username",
+        value: "admin"
+    },
+    {
+        key: "email",
+        value: "admin@gmail.com"
+    }, {
+        key: "phone",
+        value: "0123456789"
+    }]
+        - Lưu ý: value là chuỗi phải được đặt trong ngoặc đơn để tránh lỗi cú pháp SQL
+        */
+        let query = "Select * from users";
+
+        if (filter.length > 0) {
+            query += " where ";
+
+            for (let i = 0; i < filter.length; i++) {
+                if (!query.includes("and")) query += `${filter[i].key} = ${filter[i].value}`;
+                else query += `${filter[i].key} = ${filter[i].value}`;
+
+                if (i < filter.length - 1) query += " and ";
+            }
+        }
+
+        db.all(query, [], (err, rows) => {
+            if (err) {
+                console.error("Error fetching users:", err);
+                return;
+            }
+
+            let idx = 0;
+
+            do {
+                delete rows[idx].password_hash;
+                idx++;
+            } while (idx < rows.length);
+            
+            let dataExport = {
+                sheetName: "Users",
+                fileName: "users.xlsx",
+                matrix: {
+                    columns: [
+                        { header: "STT", key: "stt", width: 10 },
+                        { header: "ID", key: "id", width: 10 },
+                        { header: "Username", key: "username", width: 15 },
+                        { header: "Họ tên", key: "full_name", width: 20 },
+                        { header: "email", key: "email", width: 25 },
+                        { header: "Số điện thoại", key: "phone", width: 15 },
+                        { header: "Tỉnh/Thành", key: "province_name", width: 20 },
+                        { header: "Xã/Phường", key: "ward_name", width: 20 },
+                        { header: "Trường học", key: "school_name", width: 28 },
+                        { header: "Đơn vị công tác", key: "work_unit", width: 25 },
+                        { header: "Chức vụ", key: "organization_position", width: 15 }
+                    ],
+                    rows: rows.map((row, index) => ({ ...row, stt: index + 1 }))
+                }
+            }
+            DataModel.ExportData(dataExport, req, res);
+
+        });
+
+    }
 }
+
 
 
 module.exports = ExportController;
--- backend/models/DataModel.js
+++ backend/models/DataModel.js
@@ -15,13 +15,78 @@ class DataModel {
 
         const worksheet = workbook.addWorksheet(data.sheetName || "Sheet 1");
 
-        worksheet.columns = data.matrix.columns;
+        
 
+        worksheet.columns = data.matrix.columns;
+        worksheet.spliceRows(1, 0, []);
+        worksheet.spliceRows(1, 0, []);
+
+        worksheet.getRow(1).height = worksheet.getRow(2).height = 30;
+
+
+        const titleCell = worksheet.getCell("A1");
+
+        titleCell.value = "TRUNG ƯƠNG ĐOÀN TNCS HỒ CHÍ MINH";
+        worksheet.mergeCells("A1:K1");
+
+        const heading2 = worksheet.getCell("A2");
+
+        heading2.value = "Danh sách thống kê cuộc thi Nhật ký Hoa phượng đỏ 2026";
+        worksheet.mergeCells("A2:K2");
+
+
+
+        heading2.alignment = titleCell.alignment = {
+            horizontal: "left",
+            vertical: "middle"
+        };
+
+        heading2.font = titleCell.font = {
+            bold: true,
+            size: 12
+        };
+
+        
         for (let i = 0; i < data.matrix.rows.length; i++) {
             const row = data.matrix.rows[i];
             worksheet.addRow(row);
         }
 
+        const headerRow = worksheet.getRow(3);
+
+        headerRow.font = {
+            bold: true,
+            color: {
+                argb: "000000"
+            }
+        };
+
+        headerRow.alignment = {
+            horizontal: "center",
+            vertical: "middle",
+            wrapText: true
+        };
+
+        // Nền vàng
+        headerRow.fill = {
+            type: "pattern",
+            pattern: "solid",
+            fgColor: {
+                argb: "FFD966"
+            }
+        };
+
+        // Border header
+        headerRow.eachCell((cell) => {
+
+            cell.border = {
+                top: { style: "thin" },
+                left: { style: "thin" },
+                bottom: { style: "thin" },
+                right: { style: "thin" }
+            };
+
+        });
         /*
         let data = {
             sheetName: "",
@@ -59,6 +124,7 @@ class DataModel {
         */
 
 
+
         res.setHeader(
             "Content-Type",
             "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
@@ -69,10 +135,9 @@ class DataModel {
             'attachment; filename="Thongkecuocthi.xlsx"'
         );
 
-        
         await workbook.xlsx.write(res);
 
-        
+
         res.end();
     }
 }
